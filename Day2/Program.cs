71e31a4d8663066aa53294f5556939ae82ef51b8
Nick Verhaegen <gatzby@gmail.com>
2023-12-05 00:47:46 +0100

    Day 2 complete

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -13,6 +13,7 @@ var limits = new Dictionary<string, int>
 
 
 var totalcount1 = 0;
+var totalpower = 0;
 
 // Solution 1
 
@@ -21,21 +22,36 @@ foreach (var line in dataset)
     string[] header = line.Split(':');
     var game = int.Parse(new Regex(@"\d+").Match(header[0]).Value);//Regex.Match(header[0]);
     var validgame = true;
+    var gamescore = new Dictionary<string, int>
+    {
+        {"red", 0 },
+        {"green", 0 },
+        {"blue", 0 }
+    };
     string[] pulls = header[1].Split(";");
     foreach (var pull in pulls)
     {
+
         string[] values = pull.Split(",");
         foreach (var value in values)
         {
             string[] elements = value.Trim().Split(" ");
+
             if (int.Parse(elements[0]) > limits[elements[1]])
             {
                 validgame = false;
             }
+            if (int.Parse(elements[0]) > gamescore[elements[1]]) 
+            {
+                gamescore[elements[1]] = int.Parse(elements[0]);
+            }
         }
     }
     if (validgame) totalcount1 = totalcount1 + game;
+    totalpower = totalpower + (gamescore["red"] * gamescore["green"] * gamescore["blue"]); 
 
 }
 
+
 Console.WriteLine($"Solution 1 : {totalcount1}");
+Console.WriteLine($"Solution 2 : {totalpower}");
