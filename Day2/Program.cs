8cbeecf38e4540b9f04cc28be1a0c9550387cae1
Nick Verhaegen <gatzby@gmail.com>
2023-12-05 00:03:46 +0100

    Day 2 solution 1

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,41 @@
-﻿Console.WriteLine("Advent Of Code 2023 Day2");
+﻿using System.Text.RegularExpressions;
 
-var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2023\\Day2\\data\\test.txt");
+Console.WriteLine("Advent Of Code 2023 Day2");
 
+var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2023\\Day2\\data\\prod.txt");
+
+var limits = new Dictionary<string, int>
+{
+    {"red", 12},
+    {"green", 13},
+    {"blue", 14}
+};
+
+
+var totalcount1 = 0;
+
+// Solution 1
+
+foreach (var line in dataset)
+{
+    string[] header = line.Split(':');
+    var game = int.Parse(new Regex(@"\d+").Match(header[0]).Value);//Regex.Match(header[0]);
+    var validgame = true;
+    string[] pulls = header[1].Split(";");
+    foreach (var pull in pulls)
+    {
+        string[] values = pull.Split(",");
+        foreach (var value in values)
+        {
+            string[] elements = value.Trim().Split(" ");
+            if (int.Parse(elements[0]) > limits[elements[1]])
+            {
+                validgame = false;
+            }
+        }
+    }
+    if (validgame) totalcount1 = totalcount1 + game;
+
+}
+
+Console.WriteLine($"Solution 1 : {totalcount1}");
